2e8c812b5393e8427bda46eef46c9d92a0ca5548
Muhammad Umer <umerkang66@gmail.com>
2025-07-27 12:43:00 +0500

    complete the basic app

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import PromptTemplate
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--language", type=str, default="python")
+parser.add_argument("--tasks", type=str, default="return a list of numbers")
+args = parser.parse_args()
+
 
 llm = OllamaLLM(model="llama3.2:latest")
 
+code_prompt = PromptTemplate.from_template(
+    "Write a {language} code snippet that will {tasks}."
+)
 
-response = llm.invoke("Create a very very simple poem")
-print(response)
+code_chain = code_prompt | llm
+
+result = code_chain.invoke(
+    {
+        "language": args.language, 
+        "tasks": args.tasks
+    }
+)
+
+print(result)
