55ff19c47259a499082a9732e280c3365b7b413c
Muhammad Umer <umerkang66@gmail.com>
2025-08-04 15:46:39 +0500

    add streaming to the app

--- chat-pdf/app/chat/chat.py
+++ chat-pdf/app/chat/chat.py
@@ -1,8 +1,9 @@
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 
 
 def build_chat(chat_args: ChatArgs):
@@ -18,10 +19,12 @@ def build_chat(chat_args: ChatArgs):
     """
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
 
-    return ConversationalRetrievalChain.from_llm(
+    return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
+        condense_question_llm=condense_question_llm,
         retriever=retriever,
         memory=memory,
     )
